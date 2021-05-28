a8bf13d766ddbc6eb3630cc2e7c489cfb3b8917b
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-05-28 23:03:47 +0300

    Add class test account and a set up method

---
+++ account_test.py
@@ -0,0 +1,17 @@
+import unittest
+from account import Account
+
+
+class TestAccount(unittest.TestCase):
+
+      """
+      Test class that defines test cases for the account class behaviours.
+
+        Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+      """
+
+def setUp(self):
+  """
+  setUp method to run before each test case.
+  """
