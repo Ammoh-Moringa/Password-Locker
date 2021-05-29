948858d713d128307e70a49d0908536273f4cf7b
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-05-29 13:09:01 +0300

    Add teardown method to clean up each test test case has run.

--- credentials_test.py
+++ credentials_test.py
@@ -35,7 +35,23 @@ class TestCredentials(unittest.TestCase):
       self.new_credentials.save_credentials() # saving the new credentials
       self.assertEqual(len(Credentials.credential_list),1)  
 
+  def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            Credentials.credential_list = []    
 
+
+
+  def test_save_multiple_credentials(self):
+            '''
+            test_save_multiple_account to check if we can save multiple credentials
+            objects to our credential_list
+            '''
+            self.new_credentials.save_credentials()
+            test_credentials = Credentials("youtube","Amos","Ammoh96") # new credentials
+            test_credentials.save_credentials()
+            self.assertEqual(len(Credentials.credential_list),2)
 
 
 
